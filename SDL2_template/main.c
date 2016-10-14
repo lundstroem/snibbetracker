3afb4c96c2b8bfc9fd3cae2f77ff9bdc9ede4c92
Harry Lundström <h.lundstroem@gmail.com>
2016-10-14 15:45:46 +0200

    added windows build

--- SDL2_template/main.c
+++ SDL2_template/main.c
@@ -812,7 +812,7 @@ static char *load_file(char *path) {
             }
             fclose(fp);
         } else {
-            if(errorlog) { printf("file pointer is null\n"); }
+            if(errorlog) { printf("file not found at path %s\n", path); }
         }
     } else {
         if(errorlog) { printf("cannot load, path is null\n"); }
@@ -4676,20 +4676,24 @@ static int get_buffer_size_from_index(int i) {
 }
 
 static void copy_project_win(const char *name) {
-    char *path = cAllocatorAlloc((1024 * sizeof(char*)), "config path");
-    snprintf(path, 1023, "%s%s", conf_default_dir, name);
-    char *b = load_file(path);
+    char *read_path = cAllocatorAlloc((1024 * sizeof(char*)), "win path 1");
+    snprintf(read_path, 1023, "%s%s", "demos\\", name);
+	char *write_path = cAllocatorAlloc((1024 * sizeof(char*)), "win path 2");
+    snprintf(write_path, 1023, "%s%s", conf_default_dir, name);
+    char *b = load_file(read_path);
     if(b != NULL) {
         FILE * fp;
-        fp = fopen(path, "w+");
+        fp = fopen(write_path, "w+");
         if(fp != NULL) {
+			if(debuglog) { printf("writing file at path %s", write_path); }
             fprintf(fp, "%s", b);
             fclose(fp);
         }
     } else {
-        if(debuglog) { printf("could not load file %s", path); }
+        if(debuglog) { printf("could not load file %s", read_path); }
     }
-    cAllocatorFree(path);
+    cAllocatorFree(read_path);
+	cAllocatorFree(write_path);
 }
 
 static void load_config(void) {
@@ -4703,10 +4707,10 @@ static void load_config(void) {
         cAllocatorFree(b);
     }
     if(!success) {
-        if(debuglog) { printf("could not find config file. Writing config.txt and copying demo projects.\n"); }
+        if(debuglog) { printf("could not find config file. Writing config.txt and copying demo projects to %s\n", path); }
         bufferSize = 8192;
         FILE * fp;
-        fp = fopen("config.txt", "w+");
+        fp = fopen(path, "w+");
         if(fp != NULL) {
             fprintf(fp, "%s", "{\r\n\"buffer_size\":4096,\r\n\"buffer_size_info\":\"Must be a power of two, for example 256, 512, 1024, 2048, 4096, 8192, 16384\",\r\n\"fullscreen\":false,\r\n\"preview\":true,\r\n\"color_info_text_bg\" : {\"r\" : 0, \"g\" : 0, \"b\" : 0},\r\n\"color_file_name_text\" : {\"r\" : 250, \"g\" : 100, \"b\" : 100},\r\n\"color_inactive_instrument_node\" : {\"r\" : 100, \"g\" : 100, \"b\" : 0},\r\n\"color_active_instrument_node\" : {\"r\" : 255, \"g\" : 100, \"b\" : 0},\r\n\"color_envelope\" : {\"r\" : 0, \"g\" : 100, \"b\" : 100},\r\n\"color_visualiser\" : {\"r\" : 100, \"g\" : 250, \"b\" : 100},\r\n\"color_visualiser_clipping\" : {\"r\" : 250, \"g\" : 100, \"b\" : 100},\r\n\"color_inactive_text\" : {\"r\" : 100, \"g\" : 100, \"b\" : 100},\r\n\"color_text\" : {\"r\" : 240, \"g\" : 240, \"b\" : 240},\r\n\"color_text_bg\" : {\"r\" : 0, \"g\" : 0, \"b\" : 0},\r\n\"color_marker\" : {\"r\" : 100, \"g\" : 100, \"b\" : 0},\r\n\"color_solo\" : {\"r\" : 0, \"g\" : 0, \"b\" : 100},\r\n\"color_solo_text\" : {\"r\" : 255, \"g\" : 255, \"b\" : 255},\r\n\"color_mute\" : {\"r\" : 200, \"g\" : 0, \"b\" : 0},\r\n\"color_mute_text\" : {\"r\" : 255, \"g\" : 255, \"b\" : 255},\r\n\"color_active_row\" : {\"r\" : 0, \"g\" : 100, \"b\" : 100},\r\n\"color_active_row_text\" : {\"r\" : 0, \"g\" : 0, \"b\" : 0},\r\n\"color_playing_row\" : {\"r\" : 100, \"g\" : 250, \"b\" : 100},\r\n\"color_playing_row_text\" : {\"r\" : 0, \"g\" : 0, \"b\" : 0},\r\n\"color_edit_marker\" : {\"r\" : 255, \"g\" : 100, \"b\" : 0},\r\n\"color_edit_text\" : {\"r\" : 0, \"g\" : 0, \"b\" : 0},\r\n\"color_selection_marker\" : {\"r\" : 0, \"g\" : 255, \"b\" : 255},\r\n\"color_selection_text\" : {\"r\" : 0, \"g\" : 0, \"b\" : 0},\r\n\"color_bg\" : {\"r\" : 20, \"g\" : 20, \"b\" : 20},\r\n\"color_bg1\" : {\"r\" : 60, \"g\" : 60, \"b\" : 60},\r\n\"color_bg2\" : {\"r\" : 60, \"g\" : 60, \"b\" : 60},\r\n\"color_bg3\" : {\"r\" : 60, \"g\" : 60, \"b\" : 60},\r\n\"color_bg4\" : {\"r\" : 60, \"g\" : 60, \"b\" : 60},\r\n\"color_bg5\" : {\"r\" : 60, \"g\" : 60, \"b\" : 60},\r\n\"color_bg6\" : {\"r\" : 60, \"g\" : 60, \"b\" : 60},\r\n\"color_bg1_highlight\" : {\"r\" : 40, \"g\" : 40, \"b\" : 40},\r\n\"color_bg2_highlight\" : {\"r\" : 40, \"g\" : 40, \"b\" : 40},\r\n\"color_bg3_highlight\" : {\"r\" : 40, \"g\" : 40, \"b\" : 40},\r\n\"color_bg4_highlight\" : {\"r\" : 40, \"g\" : 40, \"b\" : 40},\r\n\"color_bg5_highlight\" : {\"r\" : 40, \"g\" : 40, \"b\" : 40},\r\n\"color_bg6_highlight\" : {\"r\" : 40, \"g\" : 40, \"b\" : 40}\r\n}\r\n");
             fclose(fp);
@@ -4723,6 +4727,7 @@ static void load_config(void) {
             copy_project_win("korvhastig.snibb");
             copy_project_win("websnacks.snibb");
             copy_project_win("wrestchest.snibb");
+			copy_project_win("projectcart.snibb");
         } else {
             if(debuglog) { printf("could not find config file after writing. path:%s\n", path); }
         }
